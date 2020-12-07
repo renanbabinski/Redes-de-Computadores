2fe1881284f5e521c7bf9b4226a17c9fe7c42737
renanbabinski <renan10babinski@hotmail.com>
2020-12-07 02:03:07 -0300

    interpretação de arquivos .config

--- Trabalho_Final/router.c
+++ Trabalho_Final/router.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #define EXIT 0
 
+FILE *configs, *enlaces;
 
 int geth(){
 	char s;
@@ -14,28 +15,59 @@ int geth(){
 	return 0;
 }
 
-int context_menu(char* router){
+int context_menu(int router){
     int menu;
     system("clear");
-    printf("Eu sou o router numero: %s \n\n", router);
+    printf("Eu sou o router numero: %d \n\n", router);
     printf("1) MANDAR MENSAGEM \n");
     printf("2) VER VIZINHOS \n");
     printf("3) VER CONFIGURAÇÕES DESSE ROUTER\n");
+    printf("4) VER MATRIZ DE ENLACES\n");
     printf("0) SAIR DO PROGRAMA\n");
     printf("\n\n");
     printf("Sua Opção:");
     scanf("%d", &menu);
-    getchar();
+    geth();
     return menu;
 }
 
+void get_router_config(int* numero, int* porta, char* ip){    //pega a linha de configuração do roteador correspondente
+    char linha[50];
+    char* substring;
+
+    while(!feof(configs)){                      
+            fgets(linha, 50, configs);  
+            if(linha[0] == ('0' + *numero))
+                break;
+        }
+    fseek(configs, 0, SEEK_SET);
+
+
+
+    substring = strtok(linha, " ");
+    for (int i = 0; i < 3; i++)
+    {
+        if (i == 1)
+            *porta = atoi(substring);
+        else if (i == 2)
+            strcpy(ip, substring);
+
+        substring = strtok(NULL, " ");
+    }
+}
+
+////////////////////////////  MAIN  ///////////////////////////////////////////
 
 int main(int argc, char *argv[]){
 
-    int menu;
-    FILE *configs, *enlaces;
-    char linha [50], routern;
-    char *result;
+    int menu, count=0;
+    char linha [50];
+    char* substring;
+
+    //Configurações do router
+    int numero_r;
+    int porta_r;
+    char ip_r[15];
 
     if (argc != 2)
     {
@@ -47,18 +79,52 @@ int main(int argc, char *argv[]){
         printf("Problemas na abertura do arquivo de configuração do roteador!\n");
         return 1;
     }
+    if((enlaces = fopen("enlaces.config", "rt")) == NULL){
+        printf("Problemas na abertura do arquivo de enlaces!\n");
+        return 1;
+    }
 
-    while(!feof(configs)){                      //pega a linha de configuração do roteador correspondente
-            fgets(linha, 50, configs);  
-            if(linha[0] == *argv[1])
-                break;
-        }
-        fseek(configs, 0, SEEK_SET);
+    numero_r = *argv[1]- 48;
+    get_router_config(&numero_r, &porta_r, ip_r);
 
-
+    while(!feof(configs)){                     
+        fgets(linha, 50, configs);
+        count++;
+    }
+    fseek(configs, 0, SEEK_SET);
     
+    int links[count][count];  //Matriz de enlaces bidirecionais
+    int j,k;
+    for(j=0; j<count; j++){
+        for(k=0; k<count; k++){
+            links[j][k] = -1;
+        }
+    }
+    
+    
+    while(!feof(enlaces)){                  
+        fgets(linha, 50, enlaces);  
+        
+        substring = strtok(linha, " ");
+        for (int i = 0; i < 3; i++)
+        {
+            if(i == 0)
+                j = atoi(substring);
+            else if (i == 1)
+                k = atoi(substring);
+            else if (i == 2){
+                links[j-1][k-1] = atoi(substring);
+                links[k-1][j-1] = atoi(substring);
+            }
+            substring = strtok(NULL, " ");
+        }
+    }
+    fseek(enlaces, 0, SEEK_SET);
 
-while ((menu = context_menu(argv[1])) != EXIT){
+
+//////////////// MENU DE PROGRAMA ///////////////////
+
+while ((menu = context_menu(numero_r)) != EXIT){
 
     switch (menu)
     {
@@ -67,17 +133,32 @@ while ((menu = context_menu(argv[1])) != EXIT){
         geth();
         break;
     case 2: 
-        printf("Você Escolheu: %d", menu);
+        printf("Ver Vizinhos\n\n");
+        for(k=0; k<count; k++){
+            if(links[numero_r-1][k] != -1){
+                printf("Vizinho numero: %d\nCusto: %d\n\n", k+1, links[numero_r-1][k]);
+            }
+        }
         geth();
         break;
     case 3:
         printf("Ver as configurações do router\n\n");
 
-        printf("Numero do Roteador: %c", linha[0]);
-        printf("Numero da porta: %s", linha[2]);
-        printf("IP: %s", linha[8]);
+        printf("Numero do Roteador: %d\n", numero_r);
+        printf("Numero da porta: %d\n", porta_r);
+        printf("IP: %s\n", ip_r);
         geth();
         break;
+    case 4:
+        printf("Ver Matriz de Enlaces\n\n");
+
+        for(j=0; j<count; j++){
+            for(k=0; k<count; k++){
+                printf("%d   ", links[j][k]);
+            }
+            printf("\n\n");
+        }
+        geth();
         
     default:
         printf("Opção Inválida\n");
